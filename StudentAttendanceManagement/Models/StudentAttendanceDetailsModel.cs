f98c2470fa758c648dd7fee132adfa9646799a9d
Rahul12217 <rahulyamali12217@gmail.com>
2023-02-27 15:09:15 +0530

    Add project files.

---
+++ StudentAttendanceManagement/Models/StudentAttendanceDetailsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentAttendanceManagement.Models
+{
+    public class StudentAttendanceDetailsModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public double AttendencePercentage { get; set; }
+
+    }
+}
